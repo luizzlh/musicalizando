253bfc046f406fda155c5299fae57f4e53e3027b
Luiz <luizzlh2004@gmail.com>
2025-12-28 10:06:02 -0300

    Início do projeto Musicalizando.

---
+++ src/main/java/com/alura/musicalizando/MusicalizandoApplication.java
@@ -0,0 +1,13 @@
+package com.alura.musicalizando;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicalizandoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicalizandoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alura/musicalizando/MusicalizandoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.musicalizando;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicalizandoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
